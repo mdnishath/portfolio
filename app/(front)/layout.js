4640ff510aabb5241fbc47f2e5a6697bf14bfab9
mdnishath <nishatbd3388@gmail.com>
2023-08-19 21:52:14 +0600

    google index

--- app/(front)/layout.js
+++ app/(front)/layout.js
@@ -2,10 +2,6 @@
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/shared/Footer";
 import React from "react";
-export const metadata = {
-  name: "google-site-verification",
-  content: "5o-vUqayTL-c9YWDGmYzfQI9MjGn0rAQtgAwM1-inSk",
-};
 
 const FrontLayout = ({ children }) => {
   return (
