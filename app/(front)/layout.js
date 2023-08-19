b521f2165434c67fc3c2e26239966c5373efd0c5
mdnishath <nishatbd3388@gmail.com>
2023-08-19 21:43:30 +0600

    webmaster tool added

--- app/(front)/layout.js
+++ app/(front)/layout.js
@@ -2,6 +2,10 @@
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/shared/Footer";
 import React from "react";
+export const metadata = {
+  name: "google-site-verification",
+  content: "5o-vUqayTL-c9YWDGmYzfQI9MjGn0rAQtgAwM1-inSk",
+};
 
 const FrontLayout = ({ children }) => {
   return (
